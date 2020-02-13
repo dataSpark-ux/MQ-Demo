029453a4d83fb206b99c498ddd4c6c63026f8663
wy <837466309@qq.com>
2020-02-13 19:14:14 +0800

    first commit

---
+++ rabbitmq/rabbit-provider/src/main/java/com/example/rabbitprovider/RabbitProviderApplication.java
@@ -0,0 +1,16 @@
+package com.example.rabbitprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangyi
+ */
+@SpringBootApplication
+public class RabbitProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitProviderApplication.class, args);
+    }
+
+}
